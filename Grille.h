11c5aca243b68519c0b6c410378a727863876b97
Avalanches-cesi <elisa.moullet@viacesi.fr>
2025-12-05 09:19:17 +0100

    Create Grille.h

---
+++ Grille.h
@@ -0,0 +1,19 @@
+
+class Grille {
+private:
+    int largeur;
+    int longueur;
+
+public:
+    Grille(int largeur, int longueur);
+    ~Grille();
+    Cellule& GetCellule(int i, int j);
+    int GetLongueur();
+    int GetLargeur();
+    int Voisins(int i, int j);
+    void InitialiserDepuisFichier (Fichier& fichier);
+    void Iteration (Regle& regle);
+    void Copier (Grille* nouvelle);
+
+
+};
